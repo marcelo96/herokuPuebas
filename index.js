8b0cb66d06a5c0e2666eeae070997a1c0a5bfb18
Marcelo <marceloriosrodriguez@gmail.com>
2020-04-16 16:50:10 -0500

    Coreccion Archivo js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => res.send('Hola mundo!!'));
+
+app.get('/inicio', function(req,res){
+	res.send('Estas en el inicio');
+});
+app.get('/panel', function(req,res){
+	res.send('Estas en el panel');
+});
+
+app.listen(3000, () => console.log('El servidor esta en http://localhost:3000'));
