e7a915009498f42a6115821c90f83a29fa2ff366
marcelo96 <41080704+marcelo96@users.noreply.github.com>
2020-04-16 14:56:54 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => res.send('Hola mundo!!'));
+
+app.get('/inicio', function(req,res){
+	res.send('Estas en el inicio');
+});
+
+app.listen(3000, () => console.log('El servidor esta en http://localhost:3000'));
